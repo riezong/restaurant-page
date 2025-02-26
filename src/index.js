e98c1cb9cfd58643319ea7d4a0225feccbbf7a0c
Andrew <71823699+riezong@users.noreply.github.com>
2025-02-26 15:07:26 +1100

    Add event listeners to nav buttons

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@
 import { initialPageLoad } from './initial-page-load';
 import { menu } from './menu';
 import { contact } from './contact';
+
+const tabSwitcher = (function () {
+	// event listeners
+	const btn = document.querySelector('nav');
+	function handleClick(event) {
+		let target = event.target;
+		switch (target.id) {
+			case 'home':
+				console.log('Home menu item was clicked');
+				break;
+			case 'menu':
+				console.log('Menu menu item was clicked');
+				break;
+			case 'about':
+				console.log('About menu item was clicked');
+				break;
+		}
+	}
+	btn.addEventListener('click', handleClick);
+
+	// clear DOM
+	// render DOM
+})();
